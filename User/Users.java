0d200ebbeac4bfcd2d4834022ee94d3942ecfcc7
GIF1 <gif1@hi.is>
2016-03-16 21:53:21 +0000

    Testing initial commit from eclipse

---
+++ User/Users.java
@@ -0,0 +1,15 @@
+public class Users extends Package{
+	private String username;
+	private Integer age;
+	private String email;
+	private String hobbies[];
+	private String password;
+	
+	private void login(User){...}
+	private void updateUser(User){...}
+	private void setAge(Integer age){...}
+	private void setEmail(String email){...}
+	private void setHobbies(String[] hobbies){...}
+	private void setPassword(String password){...}
+	private void deleteTrip(Package pac)
+}
